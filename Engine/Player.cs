6a207babdba0eff5e511994216b6fe5fe67532f4
Tanner Thompson <98563124+tlthom32@users.noreply.github.com>
2022-04-02 12:45:28 -0400

    refactored, implemented LINQ

--- Engine/Player.cs
+++ Engine/Player.cs
@@ -36,17 +36,7 @@ namespace Engine
             }
 
             // See if the player has the required item in their inventory
-            foreach (InventoryItem ii in Inventory)
-            {
-                if (ii.Details.ID == location.ItemRequiredToEnter.ID)
-                {
-                    // We found the required item, so return "true"
-                    return true;
-                }
-            }
-
-            // We didn't find the required item in their inventory, so return "false"
-            return false;
+            return Inventory.Exists(ii => ii.Details.ID == location.ItemRequiredToEnter.ID);
         }
 
         public bool HasThisQuest(Quest quest)
